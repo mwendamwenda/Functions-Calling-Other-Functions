6b2322b4a7963084dbb773fb1e1a5d94d352fc40
mwendamwenda <benmwenda83@gmail.com>
2022-11-01 15:44:00 +0300

    final prectise

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('family is the basic unit');
+// console.log('family is the basic unit');
+
+function cutFruitPieces(fruit){
+    return fruit * 4;
+}
+
+function fruitProcessor (apples,mangoes){
+   const applePieces = cutFruitPieces(apples);
+  const mangoPieces = cutFruitPieces(mangoes);
+
+    const juice =`juice with ${applePieces} pieces of apples and ${mangoPieces} pieces mangoes.`;
+    return juice;
+}
+console.log(fruitProcessor(2,3));
